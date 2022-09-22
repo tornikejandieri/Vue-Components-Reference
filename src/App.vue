18181e10864b568e26fc5c7523ec687fcb70ee30
Tornike Jandieri <108745129+tornikejandieri@users.noreply.github.com>
2022-09-23 00:28:34 +0400

    dynamic component features, slots, style scopes reference

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <TheHeader />
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <ActiveGoals v-if="selectedComponent === 'active-goals'" />
+    <ManageGoals v-if="selectedComponent === 'manage-goals'" /> -->
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+// import BadgeList from "./components/BadgeList.vue";
+// import UserInfo from "./components/UserInfo.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
+
+export default {
+  components: {
+    TheHeader,
+    ActiveGoals,
+    ManageGoals,
+  },
+  data() {
+    return {
+      selectedComponent: "active-goals",
+      activeUser: {
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+    };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    },
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/ManageGoals.vue
@@ -0,0 +1,37 @@
+<template>
+  <h2>Manage Goals</h2>
+  <input type="text" ref="goal" />
+  <button @click="setGoal">set goal</button>
+  <teleport to="body">
+    <ErrorAlert v-if="inputIsInvalid">
+      <h2>Input is invalid</h2>
+      <p>please enter at least a few characters</p>
+      <button @click="confirmError">okay</button>
+    </ErrorAlert>
+  </teleport>
+</template>
+
+<script>
+import ErrorAlert from "./ErrorAlert.vue";
+export default {
+  components: {
+    ErrorAlert,
+  },
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === "") {
+        this.inputIsInvalid = true;
+      }
+    },
+    confirmError() {
+      this.inputIsInvalid = !this.inputIsInvalid;
+    },
+  },
+};
+</script>
